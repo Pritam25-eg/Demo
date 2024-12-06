92528f6ce7bac5e6eaf9a20c77ef762430d61c15
Pritam25-eg <pritammandal389@gmail.com>
2024-12-06 11:30:32 +0530

    first commit

---
+++ demo.java
@@ -0,0 +1,27 @@
+class pritam
+{
+    int age;
+  void fuck()
+  {
+    System.out.println(" the life is very short");
+  }
+}
+
+class mandal extends pritam
+{
+  void disp()
+  {
+    System.out.println(" fucking genious");
+    System.out.println(" the fucking shit");
+  }
+}
+
+public class demo {
+    public static void main(String[] args) {
+       
+        mandal st=new mandal();
+        st.fuck();
+        st.disp();
+    }
+    
+}
